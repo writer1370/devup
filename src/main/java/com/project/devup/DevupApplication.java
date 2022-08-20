e2e8b7c8633e522bd5bd26bdd0a3bd6208288179
writer1370 <writer1370@gmail.com>
2022-08-21 04:55:59 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/devup/DevupApplication.java
@@ -0,0 +1,13 @@
+package com.project.devup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/devup/DevupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.devup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
